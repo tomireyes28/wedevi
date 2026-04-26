be81f789767d08b4b8748682a787fde0795c6d4d
tomireyes28 <tomireyes.tr@gmail.com>
2026-04-26 16:53:37 -0300

    chore: initial setup, NextJs, Tailwind and brand colors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Esta es la clave para Hostinger
+  images: {
+    unoptimized: true, // Requerido para exportación estática
+  },
 };
 
 export default nextConfig;

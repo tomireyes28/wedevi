236439d3641157862b6718f9bca90b7021be9ba3
tomireyes28 <tomireyes.tr@gmail.com>
2026-06-23 20:07:30 -0300

    fix: corrected phone number added

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Esta es la clave para Hostinger
+  output: 'export',
   images: {
-    unoptimized: true, // Requerido para exportación estática
+    unoptimized: true,
   },
 };
 
